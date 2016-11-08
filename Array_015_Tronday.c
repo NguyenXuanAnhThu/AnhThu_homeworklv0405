10469bece9516bc17fda926ded6175eb34aee046
NguyenXuanAnhThu <thutk08@gmail.com>
2016-11-08 22:55:00 +0700

    Add files via upload

---
+++ Array_015_Tronday.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int i,n;
+    printf ("Hello Boss\n");
+    printf ("Nhap gia tri cua n:=");
+    scanf  ("%d",&n);
+    int a[n]; // khai bao mang a
+    for (i=0;i<=n;i++)
+    {
+        printf("nhap cac phan tu cua mang a i:=");
+        scanf ("%d",&a[i]);
+    }
+    int b[n]; //khai bao mang b
+    for (i=0;i<=n;i++)
+    {
+        printf ("nhap cac phan tu cua mang b i:=");
+        scanf ("%d",&b[i]);
+    }
+    //tron hai day
+    int s[n];
+    for (i=0,a[i]=0;i<=n;i++)
+    {
+     s[i]=a[i]+b[i];
+    }
+    printf ("tron hai day la %d",s[i]);
+}
